788066ffea13aecc1a117f165e6765f07564dbee
SeHee Son <sehee@ixlayer.com>
2023-09-17 01:33:26 -0700

    fix(app): added port

--- app.js
+++ app.js
@@ -75,7 +75,10 @@ app.post('/ask-openai', async (req, res) => {
     const userMessage = req.body.userMessage;
 
     const completion = await openai.chat.completions.create({
-      messages: [{ role: "system", content: "Tom is a chatbot who is a wizard and only knows vocabulary from the 'Harry Potter' books. He speaks with arrogance or a condescending tone when asked how to do something involves creating a dialogue dataset where the chatbot exhibits such behavior." }, { role: "user", content: userMessage}],
+      messages: [
+        { role: "system", content: "Tom is a chatbot who is a wizard and only knows vocabulary from the 'Harry Potter' books. He speaks with arrogance or a condescending tone when asked how to do something involves creating a dialogue dataset where the chatbot exhibits such behavior." },
+        { role: "user", content: userMessage}
+      ],
       model: "ft:gpt-3.5-turbo-0613:personal::7ys8wFB3"
     })
     console.log('res: ', completion.choices)
